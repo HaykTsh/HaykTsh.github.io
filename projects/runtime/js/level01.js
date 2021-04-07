aaa3ed8eb5f0022c0d5704536483abc6bee96162
HaykTsh <hayktsh@gmail.com>
2021-04-07 17:08:46 +0000

    lol

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -47,7 +47,7 @@ var level01 = function (window) {
         }
 
         createSawBlade(200, groundY);
-        createSawBlade(400, groundY - 70);
+        createSawBlade(400, groundY - 90);
         createSawBlade(500, 175);
 
 
