e4305523483887d2b79a109d288ab1517c764e0d
HaykTsh <hayktsh@gmail.com>
2021-04-08 13:19:00 +0000

    almost done

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -18,7 +18,11 @@ var level01 = function (window) {
             "gameItems": [
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
-                { "type": "sawblade", "x": 900, "y": groundY },
+                { "type": "sawblade", "x": 800, "y": groundY },
+                { "type": "enemy", "x": 900, "y": groundY - 50},
+                { "type": "reward", "x": 500, "y": groundY - 50},
+                { "type": "health", "x": 700, "y": groundY  - 50},
+                //{ "type": "obstacle", "x": 900, "y": groundY },
             ]
         };
         window.levelData = levelData;
@@ -37,7 +41,7 @@ var level01 = function (window) {
             var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
 
             sawBladeHitZone.x = x;
-            sawBladeHitZone.y = y;
+            sawBladeHitZone.y = groundY - y;
             game.addGameItem(sawBladeHitZone);
 
             var obstacleImage = draw.bitmap('img/sawblade.png');
@@ -46,35 +50,109 @@ var level01 = function (window) {
             obstacleImage.y = -25;
         }
 
-        createSawBlade(200, groundY);
-        createSawBlade(400, groundY - 100);
-        createSawBlade(500, 175);
+        createSawBlade(200, 0);
+        createSawBlade(400, 100);
+        createSawBlade(500, 120);
 
 
         // Enemy
 
-        var enemy = game.createGameItem('enemy',25);
-        var redSquare = draw.rect(50,50,'red');
-        redSquare.x = -25;
-        redSquare.y = -25;
+        function createEnemy(x, y) { 
+
+            // Create Enemy and Sprite
+            var enemy = game.createGameItem('enemy',25);
+            var redSquare = draw.rect(50,50,'red');
+            redSquare.x = -25;
+            redSquare.y = -25;
+
             enemy.addChild(redSquare);
-        enemy.x = 400;
-        enemy.y = groundY-50;
+            
+            // Pos
+            enemy.x = x;
+            enemy.y = groundY - y;
+            
             game.addGameItem(enemy);
 
-        enemy.velocityX = -1;
-        enemy.rotationVelocity = 10;
+            // Movement
+            enemy.velocityX = -1;
+            enemy.rotationalVelocity = 10;
 
-        enemy.onPlayerCollision = function() {
-            
-            game.changeIntegrity(-10);
-        };
+            // Player and Projectile Collision
+            enemy.onPlayerCollision = function() {
 
-        enemy.onProjectileCollision = function() {
+                game.changeIntegrity(-10);
+            };
 
-            game.increaseScore(100);
-            enemy.fadeOut();
+            enemy.onProjectileCollision = function() {
+
+                game.increaseScore(100);
+                enemy.fadeOut();
+            }
         }
+
+        createEnemy(300, 50);
+        createEnemy(600, 50);
+        createEnemy(500, 50);
+
+        //Score Reward
+
+        function createReward(x, y) {
+
+            //Creation
+            var reward = game.createGameItem('reward', 10);
+            var prayer = draw.circle(10, 'blue');
+            //prayer.x = -10;
+            //prayer.y = -10;
+
+            reward.addChild(prayer);
+            
+            //Pos
+            reward.x = x;
+            reward.y = groundY- y;
+
+            game.addGameItem(reward);
+
+            //Movement
+            reward.velocityX = -1;
+
+            //Score
+            reward.onPlayerCollision = function() {
+
+                game.increaseScore(100);
+                reward.fadeOut();
+            };
+        } 
+
+        createReward(400, 50);
+
+        //Health
+
+        function createHealth(x, y) {
+
+            //Creation
+            var health = game.createGameItem('reward', 10);
+            var angel = draw.circle(10, 'green');
+
+            health.addChild(angel);
+            
+            //Pos
+            health.x = x;
+            health.y = groundY- y;
+
+            game.addGameItem(health);
+
+            //Movement
+            health.velocityX = -1;
+
+            //Health
+            health.onPlayerCollision = function() {
+
+                game.changeIntegrity(25);
+                reward.fadeOut();
+            };
+        } 
+
+        createHealth(800, 50);
 
         // DO NOT EDIT CODE BELOW HERE
     }
