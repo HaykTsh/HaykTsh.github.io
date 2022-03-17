aa123497f938d8b30a729907c08b3d5a6f373207
HaykTsh <hayktsh@gmail.com>
2022-03-17 18:51:46 +0000

    bruh

--- projects/snake/index.js
+++ projects/snake/index.js
@@ -43,14 +43,16 @@ function runProgram(){
 
 
 
-  var snake = [crSnake("head", BOARD_WIDTH, BOARD_HEIGHT), crSnake("body", BOARD_WIDTH, BOARD_HEIGHT + 20)];
+  var snake = [];
 
   var speedX = 0;
   var speedY = 0;
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keydown', keyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', keyDown);                           
+  startSnake();
+  drawSnake();
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -62,7 +64,8 @@ function runProgram(){
   */
   function newFrame() {
     
-
+    
+    reposSnake();
   }
   
   /* 
@@ -70,14 +73,67 @@ function runProgram(){
   */
   function keyDown(event) {
     
-    
+    if(event.which === KEY.W|| event.which === KEY.UP) {
+
+      speedX = 0;
+      speedY = -40;
+    }
+    if(event.which === KEY.S|| event.which === KEY.DOWN) {
+
+      speedX = 0;
+      speedY = 40;
+    }
+    if(event.which === KEY.A|| event.which === KEY.LEFT) {
+
+      speedX = -40;
+      speedY = 0;
+    }
+    if(event.which === KEY.D|| event.which === KEY.RIGHT) {
+
+      speedX = 40;
+      speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function startSnake() {
+
+    snake = [];
+
+    snake.push(crSnake("head", BOARD_WIDTH / 2, BOARD_HEIGHT / 2));
+    //snake.push(crSnake("body", BOARD_WIDTH / 2, (BOARD_HEIGHT / 2) + 40));
+
+    console.log(snake);
+  }
+
+  function reposSnake() {
+
+    var head = snake[0];
+
+    head.posX += speedX;
+    head.posY += speedY;
+
+    snake[0] = head;
+
+    drawSnake();
+  }
+
+  function drawSnake() {
+
+    for(var i = 0; i < snake.length; i++) {
+
+      var section = snake[i];
+      var segment = $('<div id="snake" class=' + section.class + '>').appendTo($('#board'));
+
+      segment.css('left', section.posX);
+      segment.css('top', section.posY);
+    }
+  }
+
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
